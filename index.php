fb82495e40936d45911d787ced101dbab1a1fb1b
biaam80 <biaamoreira80@gmail.com>
2025-05-16 18:07:10 -0300

    created book cards

--- index.php
+++ index.php
@@ -7,15 +7,15 @@
 
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
-<body class="bg-stone-900 text-gray-200">
+<body class="bg-indigo-100 text-indigo-900">
 
-    <header class="bg-stone-800">
+    <header class="bg-indigo-200">
 
-        <nav class="mx-auto max-w-screen-lg flex justify-between px-8 py-4">
+        <nav class="mx-auto max-w-screen-lg flex justify-between py-4">
             <div class="font-bold text-xl tracking-wide">Bookshelf</div>
             
             <ul class="flex space-x-4">
-                <li><a href="" class="text-violet-300 font-bold">Explorar</a></li>
+                <li><a href="" class="text-indigo-400 font-bold">Explorar</a></li>
                 <li><a href="/meus-livros.php" class="hover:underline">Meus Livros</a></li>
             </ul>
             <ul>
@@ -25,9 +25,92 @@
 
     </header>
 
-    <main>
+    <main class="mx-auto max-w-screen-lg space-y-10">
 
-        lista de livros
+        <form class="w-full flex space-x-2 mt-6">
+            <input 
+                type="text" 
+                class="rounded-md bg-indigo-200 text-sm focus:outline-none px-2 py-1"
+                placeholder="Pesquisar..."
+            >
+            <button type="submit">🔎</button>
+        </form>
+
+        <section class="grid grid-cols-1 flex gap-4 md:grid-cols-2 lg:grid-cols-3">
+            
+            <!-- livro -->
+            <div class="bg-indigo-200 p-2 rounded">
+                <div class="flex">
+                    <div class="w-1/3">img</div>
+                    <div>
+                        <a href="/livro.php?" class="hover:underline"><div class="font-semibold">titulo</div></a>
+                        <div class="text-xs italic">autor</div>
+                        <div class="text-xs italic">⭐⭐⭐⭐⭐ 4 avaliações</div>
+                    </div>
+                </div>
+                <div class="text-sm">
+                    descricao
+                </div>
+            </div>
+            <!-- livro -->
+            <div class="bg-indigo-200 p-2 rounded">
+                <div class="flex">
+                    <div class="w-1/3">img</div>
+                    <div>
+                        <a href="/livro.php?" class="hover:underline"><div class="font-semibold">titulo</div></a>
+                        <div class="text-xs italic">autor</div>
+                        <div class="text-xs italic">⭐⭐⭐⭐⭐ 4 avaliações</div>
+                    </div>
+                </div>
+                <div class="text-sm">
+                    descricao
+                </div>
+            </div>            
+            <!-- livro -->
+            <div class="bg-indigo-200 p-2 rounded">
+                <div class="flex">
+                    <div class="w-1/3">img</div>
+                    <div>
+                        <a href="/livro.php?" class="hover:underline"><div class="font-semibold">titulo</div></a>
+                        <div class="text-xs italic">autor</div>
+                        <div class="text-xs italic">⭐⭐⭐⭐⭐ 4 avaliações</div>
+                    </div>
+                </div>
+                <div class="text-sm">
+                    descricao
+                </div>
+            </div>
+            <!-- livro -->
+            <div class="bg-indigo-200 p-2 rounded">
+                <div class="flex">
+                    <div class="w-1/3">img</div>
+                    <div>
+                        <a href="/livro.php?" class="hover:underline"><div class="font-semibold">titulo</div></a>
+                        <div class="text-xs italic">autor</div>
+                        <div class="text-xs italic">⭐⭐⭐⭐⭐ 4 avaliações</div>
+                    </div>
+                </div>
+                <div class="text-sm">
+                    descricao
+                </div>
+            </div>           
+            <!-- livro -->
+            <div class="bg-indigo-200 p-2 rounded">
+                <div class="flex">
+                    <div class="w-1/3">img</div>
+                    <div>
+                        <a href="/livro.php?" class="hover:underline"><div class="font-semibold">titulo</div></a>
+                        <div class="text-xs italic">autor</div>
+                        <div class="text-xs italic">⭐⭐⭐⭐⭐ 4 avaliações</div>
+                    </div>
+                </div>
+                <div class="text-sm">
+                    descricao
+                </div>
+            </div>                         
+            
+
+        </section>
 
     </main>
 
