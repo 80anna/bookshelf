fa5ee56b6ecab91e15728b43ea6227f0ae2c68b5
biaam80 <biaamoreira80@gmail.com>
2025-05-16 15:10:43 -0300

    Create header

--- index.php
+++ index.php
@@ -4,8 +4,32 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bookshelf</title>
+
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
-<body>
-    
+<body class="bg-stone-900 text-gray-200">
+
+    <header class="bg-stone-800">
+
+        <nav class="mx-auto max-w-screen-lg flex justify-between px-8 py-4">
+            <div class="font-bold text-xl tracking-wide">Bookshelf</div>
+            
+            <ul class="flex space-x-4">
+                <li><a href="" class="text-violet-300 font-bold">Explorar</a></li>
+                <li><a href="/meus-livros.php" class="hover:underline">Meus Livros</a></li>
+            </ul>
+            <ul>
+                <li><a href="/login.php" class="hover:underline">Login</a></li>
+            </ul>
+        </nav>
+
+    </header>
+
+    <main>
+
+        lista de livros
+
+    </main>
+
 </body>
 </html>
